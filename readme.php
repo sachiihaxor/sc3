3a3d22c028ee75da5a0b58efab8e80d8e737f06f
sachiihaxor <106025611+sachiihaxor@users.noreply.github.com>
2022-09-06 14:40:47 +0700

    Create readme.php

---
+++ readme.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<title>Wh00ps - Stoupid! </title>
+<meta name='description' content='I am About Fucking!'>
+<link rel='icon' href='https://i.pinimg.com/280x280_RS/54/3b/3f/543b3f1fcd313bf31ba9a9a9355ba475.jpg'>
+<body bgcolor="black" oncontextmenu="return false" onkeydown="return false">
+<style>
+body{cursor:url("http:///foro.elhacker.net/elhacker.cur"),auto;}html{display:table;height:100%;width:100%;}body{display:table-row;}body{display:table-cell;vertical-align:middle;text-align:center;}a:link{text-decoration:none;}
+
+@import url('https://fonts.googleapis.com/css2?family=Russo+One&display=swap');
+</style>
+<link href="https://fonts.googleapis.com/css?family=Iceland" rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+<br>
+<img src='https://i.pinimg.com/280x280_RS/54/3b/3f/543b3f1fcd313bf31ba9a9a9355ba475.jpg'>
+<br>
+<font face="Russo One" style="color:white;text-shadow:0px 1px 5px white;font-size:55px">Wh00ps - Stoupid! 
+
+
+
+<font face="Iceland" style="color:white;text-shadow:0px 1px 1px white;font-size:20px">
+<br>
+<font face="Iceland" style="color:white">#SadPeople :/</font>
+<br>
+- BHI OFFICIAL - IDIOT BLACKHAT - SenpaiXCrew - Jakarta Blackhat - Jakarta Cyber Team <br>
+
+<video controls="" autoplay="" name="media"><source src="https://bema.gov.gh/persisdimatamu3.mp3" type="audio/mpeg"></video>
